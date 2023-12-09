b02341b248dd7c10461971ad0f5f342025ea3891
Jania Vandevoorde <98466424+janiavdv@users.noreply.github.com>
2023-12-09 11:35:14 -0500

    docs: update documentation of `get_books()`

--- books.R
+++ books.R
@@ -2,35 +2,33 @@
 library(jsonlite)
 
 get_books = function() {
-  #### Data Cleaning ####
+  #'@description
+    #'outputs the clean book data frame
+  #'@return a dataframe with 10,000 rows of book data
+  
   books = read.csv('https://raw.githubusercontent.com/malcolmosh/goodbooks-10k/master/books_enriched.csv')
   
   # Preprocess the 'genres' column to make it valid JSON
   books$genres = gsub("'", "\"",     books$genres)
   books$genres = gsub("\\[\\[", "[", books$genres)
   books$genres = gsub("\\]\\]", "]", books$genres)
-  
   # Convert the 'genres' column to a list
   books$genres = lapply(books$genres, fromJSON)
   
-  # Preprocess the 'genres' column to make it valid JSON
+  # Preprocess the 'authors' column to make it valid JSON
   books$authors = gsub(' "', ' \'', books$authors)
   books$authors = gsub('" ', '\' ', books$authors)
   books$authors = gsub("\\['\\[", "\\[\'", books$authors)
   books$authors = gsub("\\]'\\]", "\'\\]", books$authors)
-  
   books$authors = gsub("\\['", "\\[\"", books$authors)
   books$authors = gsub("'\\]", "\"\\]", books$authors)
   books$authors = gsub("',", "\",", books$authors)
   books$authors = gsub(", '", ", \"", books$authors)
-  
-  # Convert the 'genres' column to a list
+  # Convert the 'authors' column to a list
   books$authors = lapply(books$authors, fromJSON)
   
   # # Add Boolean genre columns
   all_books$is_nonfiction = sapply(all_books$genres, function(genres) 'nonfiction' %in% genres)
-  # books$is_ya = sapply(books$genres, function(genre_list) 'young-adult' %in% genre_list)
-  # books$is_fiction = sapply(books$genres, function(genre_list) 'fiction' %in% genre_list)
   
   return(books)
 }
